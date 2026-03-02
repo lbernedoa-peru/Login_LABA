54ecacf243622fff11cb01a5854eabe5b9672a01
Daniel Calla Castillo <danielcallacastillo7u7@gmail.com>
2026-03-01 22:02:40 -0500

    10:00 domingo

--- app/src/main/java/com/example/login_laba/MainActivity.java
+++ app/src/main/java/com/example/login_laba/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.login_laba;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button registrar_abogado;
+    Button listaAbogados;
+    Button editar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        registrar_abogado = findViewById(R.id.registrar);
+        listaAbogados = findViewById(R.id.listaAbogados);
+        editar =findViewById(R.id.editar);
+
+        registrar_abogado.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RegistrarActivity.class);
+            startActivity(intent);
+        });
+
+        listaAbogados.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ListarActivity.class);
+            startActivity(intent);
+        });
+
+        editar.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, EditarActivity.class);
+            startActivity(intent);
         });
     }
 }
---
+++ app/src/main/java/com/example/login_laba/RegistrarActivity.java
@@ -0,0 +1,15 @@
+package com.example.login_laba;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registrar);
+    }
+}
